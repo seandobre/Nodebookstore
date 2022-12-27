314460d937ba6842f0d975a2c3a3359445ef3e41
seandobre <113632554+seandobre@users.noreply.github.com>
2022-12-26 16:20:58 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express'); // requre the express framework
+var app = express();
+var fs = require('fs');
+app.use(express.json())
+PORT = process.env.PORT || 3000
+
+app.get('/books', function(req, res){
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+        res.end(data); // you can also use res.send()
+    });
+})
+//Endpoint to get a single user by id
+app.get('/book/:id', function (req, res) {
+    // First retrieve existing user list
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       var users = JSON.parse( data );
+       var user = users[req.params.id]
+       res.end( JSON.stringify(user));
+    });
+ })
+app.listen(
+    PORT,
+    () => console.log("Server is running on port",PORT)
+)
